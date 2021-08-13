1dd70d3cdf3e67c1d250b77af2a0e70831a44862
Buddhima <bgunathilaka097@gmail.com>
2021-08-13 20:51:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/nexus/MadBudgetingApp/Activities/MonthSpendingActivity.java
@@ -0,0 +1,16 @@
+package com.nexus.MadBudgetingApp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nexus.MadBudgetingApp.R;
+
+public class MonthSpendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month_spending);
+    }
+}
